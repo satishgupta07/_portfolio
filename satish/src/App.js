fdff1d37bb60ff3cdbb069d74fb92ab665943575
Satish Kumar Gupta <satishgupta.cse01@gmail.com>
2023-04-23 19:44:28 +0530

    Services Section
    Signed-off-by: Satish Kumar Gupta <satishgupta.cse01@gmail.com>

---
+++ satish/src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+import Header from './components/Header/Header';
+import Home from './components/Home/Home';
+import About from './components/About/About';
+import Skills from './components/Skills/Skills';
+import Services from './components/Services/Services';
+
+function App() {
+  return (
+    <>
+      <Header />
+      <main className='main'>
+        <Home />
+        <About />
+        <Skills />
+        <Services />
+      </main>
+    </>
+  );
+}
+
+export default App;
