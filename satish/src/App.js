707fdf6b594b5f7e333691b0d6d39eff5e941784
Satish Kumar Gupta <satishgupta.cse01@gmail.com>
2023-06-24 08:33:43 +0530

    Qualification Section
    Signed-off-by: Satish Kumar Gupta <satishgupta.cse01@gmail.com>

--- satish/src/App.js
+++ satish/src/App.js
@@ -4,6 +4,7 @@ import Home from './components/Home/Home';
 import About from './components/About/About';
 import Skills from './components/Skills/Skills';
 import Services from './components/Services/Services';
+import Qualification from './components/Qualification/Qualification';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
         <About />
         <Skills />
         <Services />
+        <Qualification />
       </main>
     </>
   );
