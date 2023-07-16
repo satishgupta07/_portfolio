a2305a5295fc79458631cefcda89a4f3ee5b7188
Satish Kumar Gupta <satishgupta.cse01@gmail.com>
2023-07-16 16:36:25 +0530

    Footer Section
    Signed-off-by: Satish Kumar Gupta <satishgupta.cse01@gmail.com>

--- satish/src/App.js
+++ satish/src/App.js
@@ -7,6 +7,7 @@ import Services from './components/Services/Services';
 import Qualification from './components/Qualification/Qualification';
 import Testimonials from './components/Testimonials/Testimonials';
 import Contact from './components/Contact/Contact';
+import Footer from './components/Footer/Footer';
 
 function App() {
   return (
@@ -20,6 +21,7 @@ function App() {
         <Qualification />
         <Testimonials />
         <Contact />
+        <Footer />
       </main>
     </>
   );
---
+++ satish/src/components/Footer/Footer.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import "./Footer.css"
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+        <div className="footer__container container">
+            <h1 className="footer__title">Satish</h1>
+
+            <ul className="footer__list">
+                <li>
+                    <a href="#about" className="footer__link">About</a>
+                </li>
+
+                <li>
+                    <a href="#portfolio" className="footer__link">Projects</a>
+                </li>
+
+                <li>
+                    <a href="#testimonials" className="footer__link">Testimonials</a>
+                </li>
+            </ul>
+
+            <div className="footer__social">
+                <a href="https://www.facebook.com/" className="footer__social-link" target='_blank'>
+                    <i className="bx bxl-facebook"></i>
+                </a>
+                <a href="https://instagram.com/" className="footer__social-link" target='_blank'>
+                    <i className="bx bxl-instagram"></i>
+                </a>
+                <a href="https://twitter.com/" className="footer__social-link" target='_blank'>
+                    <i className="bx bxl-twitter"></i>
+                </a>
+            </div>
+
+            <span className="footer__copy">&#169; SatishKumarGupta. All rights reserved</span>
+        </div>
+    </footer>
+  )
+}
+
+export default Footer
