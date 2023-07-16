0ba790b6a7aecb5504a6912dd213afbe8a4ffa52
Satish Kumar Gupta <satishgupta.cse01@gmail.com>
2023-07-16 16:01:37 +0530

    Send email using EmailJS
    Signed-off-by: Satish Kumar Gupta <satishgupta.cse01@gmail.com>

--- satish/src/components/Contact/Contact.jsx
+++ satish/src/components/Contact/Contact.jsx
@@ -1,7 +1,23 @@
-import React from 'react'
+import React, { useRef } from 'react';
+import emailjs from '@emailjs/browser';
 import "./Contact.css"
 
 const Contact = () => {
+    const form = useRef();
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+
+        emailjs.sendForm('service_16dec5r', 'template_hl3e8qw', form.current, 'Nws0Qk3XZ4xvICOJH')
+        .then((result) => {
+            console.log(result.text);
+        }, (error) => {
+            console.log(error.text);
+        });
+
+        e.target.reset();
+    };
+
   return (
     <section className="contact section" id="contact">
         <h2 className="section__title">Get in touch</h2>
@@ -50,7 +66,7 @@ const Contact = () => {
             <div className="contact__content">
                 <h3 className="contact__title">Write me your project</h3>
 
-                <form className="contact__form">
+                <form ref={form} onSubmit={sendEmail} className="contact__form">
                     <div className="contact__form-div">
                         <label className="contact__form-tag">Name</label>
                         <input type="text" name='name' className='contact__form-input'
