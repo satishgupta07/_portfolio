aa2bf54016ea6eb4f8d17c67810735f57558f609
Satish Kumar Gupta <satishgupta.cse01@gmail.com>
2024-02-22 00:43:00 +0530

    Updated the portfolio section

--- src/components/Work/Data.jsx
+++ src/components/Work/Data.jsx
@@ -1,14 +1,15 @@
 import Work1 from "../../assests/flavorFiesta.gif";
-// import Work2 from "../../assests/work2.jpg";
+import Work2 from "../../assests/work2.jpg";
 import Work3 from "../../assests/quizAdda.gif";
-// import Work4 from "../../assests/work4.jpg";
+import Work4 from "../../assests/work4.jpg";
+import Work5 from "../../assests/work5.jpg";
 
 export const projectsData = [
   {
     id: 1,
     image: Work1,
     title: "FlavorFiesta - Food Ordering WebApp",
-    category: "web",
+    category: "fullstack",
     link: "https://flavor-fiesta-frontend.vercel.app/",
     gitHubLink: "https://github.com/satishgupta07/FlavorFiestaFrontend",
   },
@@ -17,7 +18,7 @@ export const projectsData = [
     image:
       "https://res.cloudinary.com/satish07/image/upload/v1704819862/j6l70vvnucne6i1iexmn.png",
     title: "Explorer - Social Media WebApp",
-    category: "app",
+    category: "fullstack",
     link: "https://explorer-frontend-one.vercel.app/",
     gitHubLink: "https://github.com/satishgupta07/Explorer",
   },
@@ -25,7 +26,7 @@ export const projectsData = [
     id: 3,
     image: Work3,
     title: "QuizAdda",
-    category: "app",
+    category: "fullstack",
     link: "https://quiz-adda-frontend.vercel.app/login",
     gitHubLink: "https://github.com/satishgupta07/QuizAdda",
   },
@@ -34,17 +35,56 @@ export const projectsData = [
     image:
       "https://res.cloudinary.com/satish07/image/upload/v1704820188/kwztcfmq1ma0gfjrktv9.png",
     title: "ByteCanvas - A blogging web app",
-    category: "web",
+    category: "fullstack",
     link: "https://byte-canvas.vercel.app/",
     gitHubLink: "https://github.com/satishgupta07/ByteCanvas",
   },
   {
     id: 5,
+    image: Work2,
+    title: "WeatherApp",
+    category: "frontend",
+    link: "https://satish-weather-app.netlify.app/",
+    gitHubLink: "https://github.com/satishgupta07/WeatherApp",
+  },
+  {
+    id: 6,
     image:
       "https://res.cloudinary.com/satish07/image/upload/v1704819862/vy3thblpx8grn3ore7v3.png",
     title: "UnifiedMall - Flipkart Clone",
-    category: "app",
+    category: "fullstack",
     link: "https://unified-mall-frontend.vercel.app/",
     gitHubLink: "https://github.com/satishgupta07/UnifiedMall",
+  },
+  {
+    id: 7,
+    image: Work4,
+    title: "Bookshelf",
+    category: "frontend",
+    link: "https://bookshelf07.netlify.app/",
+    gitHubLink: "https://github.com/satishgupta07/Bookshelf",
+  },
+  {
+    id: 8,
+    image: Work5,
+    title: "Blogging App",
+    category: "backend",
+    link: "https://github.com/satishgupta07/Blogging_Application",
+    gitHubLink: "https://github.com/satishgupta07/Blogging_Application",
+  },
+];
+
+export const projectsNav = [
+  {
+    name: "all",
+  },
+  {
+    name: "fullstack",
+  },
+  {
+    name: "frontend",
+  },
+  {
+    name: "backend",
   },
 ];
--- src/components/Work/Works.jsx
+++ src/components/Work/Works.jsx
@@ -1,21 +1,53 @@
-import React, { useEffect, useState } from 'react';
-import { projectsData } from './Data';
-import WorkItems from './WorkItems';
+import React, { useEffect, useState } from "react";
+import { projectsData, projectsNav } from "./Data";
+import WorkItems from "./WorkItems";
 
 const Works = () => {
-    const [projects, setProjects] = useState([]);
+  const [item, setItem] = useState({ name: "all" });
+  const [projects, setProjects] = useState([]);
+  const [active, setActive] = useState(0);
 
-    useEffect(() => {
-        setProjects(projectsData);
-    }, []);
+  useEffect(() => {
+    if (item.name === "all") {
+      setProjects(projectsData);
+    } else {
+      const newProjects = projectsData.filter((project) => {
+        return project.category.toLowerCase() === item.name;
+      });
+      setProjects(newProjects);
+    }
+  }, [item]);
 
-    return (
-        <div className="work__container container grid">
-            {projects.map((item) => {
-                return <WorkItems item={item} key={item.id} />;
-            })}
-        </div>
-    );
+  const handleClick = (e, index) => {
+    setItem({ name: e.target.textContent.toLowerCase() });
+    setActive(index);
+  };
+
+  return (
+    <>
+      <div className="work__filters">
+        {projectsNav.map((item, index) => {
+          return (
+            <span
+              onClick={(e) => {
+                handleClick(e, index);
+              }}
+              className={`${active === index ? "active-work" : ""} work__item`}
+              key={index}
+            >
+              {item.name}
+            </span>
+          );
+        })}
+      </div>
+
+      <div className="work__container container grid">
+        {projects.map((item) => {
+          return <WorkItems item={item} key={item.id} />;
+        })}
+      </div>
+    </>
+  );
 };
 
 export default Works;
