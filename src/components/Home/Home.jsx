a7d3ad8d26f61f2150cc0f05f40404d52b247e62
Satish Kumar Gupta <satishgupta.cse01@gmail.com>
2026-05-04 22:09:36 +0530

    fixed import

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { motion, AnimatePresence } from "framer-motion";
+import { motion } from "framer-motion";
 import { FaGithub, FaLinkedinIn, FaTwitter, FaInstagram } from "react-icons/fa";
 import "./Home.css";
 import profileImg from "../../assests/satish_portfolio.jpg";
